238d52092b54e18a0403acb0cc04f49ba98f195d
MT <mtesseracttech@gmail.com>
2020-01-04 02:08:48 +0100

    Added other callbacks (no Input implementatino yet)

--- src/gfx/window/WindowCallbackManager.cpp
+++ src/gfx/window/WindowCallbackManager.cpp
@@ -6,35 +6,41 @@
 #include <aux/logging/Logger.hpp>
 #include "WindowCallbackManager.hpp"
 
-namespace mt::gfx{
-	void WindowCallbackManager::register_window(std::weak_ptr<gfx::ContextWindow> window) {
-	    if(!window.expired()){
-			auto win = window.lock();
+namespace mt::gfx {
+    void WindowCallbackManager::register_window(std::weak_ptr<gfx::ContextWindow> window) {
+        if (!window.expired()) {
+            auto win = window.lock();
             aux::Logger::log("Registering Callbacks for Window '" + win->get_title() + "'", aux::LogType::Info);
             glfwSetWindowUserPointer(win->get_window(), this);
-			//Setting the general GLFW error callback
+            //Setting the general GLFW error callback
             glfwSetErrorCallback(window_error_callback);
             //Setting the GLFW FrameBuffer Resize callback, that is called whenever the window changes sizes
-            glfwSetFramebufferSizeCallback(win->get_window(), fb_resize_callback);
-			window_callbacks[win->get_window()] = window;
-		}
-	}
+            glfwSetFramebufferSizeCallback(win->get_window(), framebuffer_resize_callback);
+            //Cursor Position Callback
+            glfwSetCursorPosCallback(win->get_window(), mouse_pos_callback);
+            //Mouse Button Callback
+            glfwSetMouseButtonCallback(win->get_window(), mouse_button_callback);
+            //Keyboard Callback
+            glfwSetKeyCallback(win->get_window(), keyboard_callback);
+            window_callbacks[win->get_window()] = window;
+        }
+    }
 
     void WindowCallbackManager::deregister_window(std::weak_ptr<gfx::ContextWindow> window) {
-        if(!window.expired()) {
+        if (!window.expired()) {
             auto win = window.lock();
             aux::Logger::log("Deregistering Callbacks for Window '" + win->get_title() + "'", aux::LogType::Info);
             window_callbacks.erase(win->get_window());
         }
     }
 
-    void WindowCallbackManager::fb_resize_callback(GLFWwindow *window, int width, int height) {
-        if(window){
-            auto callback_manager = (WindowCallbackManager*)glfwGetWindowUserPointer(window);
-            auto it = callback_manager->window_callbacks.find(window);
-            if(it != callback_manager->window_callbacks.end()){
+    void WindowCallbackManager::framebuffer_resize_callback(GLFWwindow *window, int width, int height) {
+        auto manager = get_callback_manager(window);
+        if (manager) {
+            auto it = manager.value().window_callbacks.find(window);
+            if (it != manager.value().window_callbacks.end()) {
                 auto wp = it->second;
-                if(!wp.expired()){
+                if (!wp.expired()) {
                     auto win = wp.lock();
                     win->framebuffer_resized(width, height);
                 }
@@ -42,14 +48,38 @@ namespace mt::gfx{
         }
     }
 
-    void WindowCallbackManager::window_error_callback(int error_code, const char * error_message) {
-
-	    std::stringstream ss;
-	    ss << "[GLFW-" << error_code << "] " << error_message;
+    void WindowCallbackManager::window_error_callback(int error_code, const char *error_message) {
+        std::stringstream ss;
+        ss << "[GLFW-" << error_code << "] " << error_message;
         aux::Logger::log(ss, aux::LogType::Error);
     }
 
-    WindowCallbackManager::WindowCallbackManager() = default;
+    void WindowCallbackManager::keyboard_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+        auto manager = get_callback_manager(window);
+        if (manager) {
+            //aux::Logger::log("Keyboard Event Handling Not Implemented Yet", aux::LogType::Info);
+        }
+    }
 
-    WindowCallbackManager::~WindowCallbackManager() = default;
+    void WindowCallbackManager::mouse_button_callback(GLFWwindow *window, int button, int action, int mods) {
+        auto manager = get_callback_manager(window);
+        if (manager) {
+            //aux::Logger::log("Mouse Button Event Handling Not Implemented Yet", aux::LogType::Info);
+        }
+    }
+
+    void WindowCallbackManager::mouse_pos_callback(GLFWwindow *window, double x, double y) {
+        auto manager = get_callback_manager(window);
+        if (manager) {
+            //aux::Logger::log("Mouse Pos Event Handling Not Implemented Yet", aux::LogType::Info);
+        }
+    }
+
+    std::optional<WindowCallbackManager> WindowCallbackManager::get_callback_manager(GLFWwindow *window) {
+        if(window){
+            auto manager = glfwGetWindowUserPointer(window);
+            if(manager) return *reinterpret_cast<WindowCallbackManager *>(manager);
+        }
+        return {};
+    }
 }
--- src/gfx/window/WindowCallbackManager.hpp
+++ src/gfx/window/WindowCallbackManager.hpp
@@ -6,6 +6,7 @@
 #define VALCANO_WINDOWCALLBACKMANAGER_HPP
 
 #include <memory>
+#include <optional>
 #include <map>
 #include "GLFW/glfw3.h"
 #include "vulkan/vulkan.hpp"
@@ -14,14 +15,19 @@
 namespace mt::gfx{
 	class WindowCallbackManager {
 	    std::map<GLFWwindow*, std::weak_ptr<gfx::ContextWindow>> window_callbacks;
-		//std::map<GLFWWindow*, std::shared_ptr<InputManager>> mouse_callback;
-		//std::map<GLFWWindow*, std::shared_ptr<InputManager>> keyboard_callback;
-		static void fb_resize_callback(GLFWwindow* window, int width, int height);
+		//std::map<GLFWWindow*, std::weak_ptr<InputManager>> mouse_callback;
+		//std::map<GLFWWindow*, std::weak_ptr<InputManager>> keyboard_callback;
+		static void framebuffer_resize_callback(GLFWwindow* window, int width, int height);
+        static void mouse_pos_callback(GLFWwindow* window,double x,double y);
+        static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+        static void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+        static std::optional<WindowCallbackManager> get_callback_manager(GLFWwindow* window);
 
 		static void window_error_callback(int, const char *);
 	public:
-	    WindowCallbackManager();
-	    ~WindowCallbackManager();
+	    WindowCallbackManager() = default;
+	    ~WindowCallbackManager() = default;
 
 	    void register_window(std::weak_ptr<gfx::ContextWindow> window);
 	    void deregister_window(std::weak_ptr<gfx::ContextWindow> window);
